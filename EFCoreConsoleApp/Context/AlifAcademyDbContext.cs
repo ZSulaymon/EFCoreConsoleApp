6d8c5ac017dde3ea52dd42388284ada58db7bffa
SulaymonZafarov <sulaymonwork@gmail.com>
2021-10-22 18:01:07 +0500

    Add project files.

---
+++ EFCoreConsoleApp/Context/AlifAcademyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCoreConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreConsoleApp.Context
+{
+    public class AlifAcademyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data source=localhost; Initial catalog=AcademySummer; Integrated security=true");
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
